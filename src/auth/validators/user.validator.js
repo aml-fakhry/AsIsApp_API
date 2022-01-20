7f1d4cc1f3e91e8e5a9b73efdbcdad704e424c3a
aml-fakhry <amlfakhry13@gmail.com>
2022-01-20 15:48:51 +0200

    '

--- src/auth/validators/user.validator.js
+++ src/auth/validators/user.validator.js
@@ -4,14 +4,14 @@ export const userSchema = {
     name: {
       type: 'string',
       format: 'email',
-      maxLength: 3,
+      maxLength: 50,
       minimum: 1,
       errorMessage: {
         // In here must be errorMessage not errorMessages
         type: 'password must be an string', // Your Custom Error Message
         format: 'formate ',
         minimum: 'password length should be a number not less than or equal to 1, current value is ${/size} ',
-        maxLength: 'password length should be a number bigger than or equal to 1, current value is ${/size}',
+        maxLength: 'password length should be a number bigger than or equal to 50, current value is ${/size}',
       },
     },
     password: {
