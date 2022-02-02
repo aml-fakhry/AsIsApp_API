74719ca1765c7a77c8d937c05490e8353df55454
aml-fakhry <amlfakhry13@gmail.com>
2022-02-02 14:55:55 +0200

    ffgdfg

--- src/auth/model/user.model.js
+++ src/auth/model/user.model.js
@@ -3,6 +3,8 @@ const userSchema = Schema({
   name: { type: String },
   email: { type: String },
   password: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date },
   userRoleId: { type: Schema.Types.ObjectId, ref: 'UserRole' },
 });
 export default model('User', userSchema);
