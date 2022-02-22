30e797772602a8f396a33f46c63876579cdfec45
Aml Fakhri <amlfakhry13@gmail.com>
2022-02-22 16:57:16 +0200

    fix pattarn.

--- src/data/security/users/validator/user.validator.js
+++ src/data/security/users/validator/user.validator.js
@@ -5,11 +5,13 @@ export const userSchema = {
       type: 'string',
       maxLength: 50,
       minLength: 2,
+      /*   pattern: '^[A-Z][a-zA-Z]+$', */
       errorMessage: {
         // In here must be errorMessage not errorMessages
         type: 'User ame must be an string', // Your Custom Error Message
         minLength: 'User name length should be a number not less than or equal to 1, current value is ${/size} ',
         maxLength: 'User name length should be a number bigger than or equal to 50, current value is ${/size}',
+        /*      pattern: 'User name cant match pattern', */
       },
     },
     email: {
